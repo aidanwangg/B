1e756a0cb3b4fb0861303ac610eb7c7ea870f0ac
Aidan Wang <aidan.wang@mail.utoronto.ca>
2025-02-09 21:13:03 -0500

    Initial commit

---
+++ blocks/a2/goal.py
@@ -0,0 +1,223 @@
+"""Objective/goal"""
+
+from __future__ import annotations
+import random
+from block import Block
+from settings import colour_name, COLOUR_LIST
+
+
+def generate_goals(num_goals: int) -> list[Goal]:
+    """Return a randomly generated list of goals with length <num_goals>.
+
+    Each goal must be randomly selected from the two types of Goals provided
+    and must have a different randomly generated colour from COLOUR_LIST.
+    No two goals can have the same colour.
+
+    Preconditions:
+    - num_goals <= len(COLOUR_LIST)
+    """
+    colours = COLOUR_LIST.copy()
+    final = []
+    for _ in range(0, num_goals):
+        colour = random.choice(colours)
+        colours.remove(colour)
+        # 1 = BlobGoal
+        # 2 = PerimeterGoal
+        goal_type = random.randint(1, 2)
+        if goal_type == 1:
+            final.append(BlobGoal(colour))
+        else:
+            final.append(PerimeterGoal(colour))
+    return final
+
+
+def flatten(block: Block) -> list[list[tuple[int, int, int]]]:
+    """Return a two-dimensional list representing <block> as rows and columns of
+    unit cells.
+
+    Return a list of lists L, where,
+    for 0 <= i, j < 2^{max_depth - self.level}
+        - L[i] represents column i and
+        - L[i][j] represents the unit cell at column i and row j.
+
+    Each unit cell is represented by a tuple of 3 ints, which is the colour
+    of the block at the cell location[i][j].
+
+    L[0][0] represents the unit cell in the upper left corner of the Block.
+    """
+    final = []
+    size = 2 ** (block.max_depth - block.level)
+    unit_size = block.size // size
+    for _ in range(size):
+        final.append([None] * size)
+
+    if block.colour is not None:
+        for i in range(size):
+            for j in range(size):
+                final[j][i] = block.colour
+        return final
+
+    for child in block.children:
+        temp = flatten(child)
+        for i in range(len(temp)):
+            for j in range(len(temp)):
+                x = (child.position[0] - block.position[0]) // unit_size + j
+                y = (child.position[1] - block.position[1]) // unit_size + i
+                final[x][y] = temp[j][i]
+    return final
+
+
+class Goal:
+    """A player goal in the game of Blocky.
+
+    This is an abstract class. Only child classes should be instantiated.
+
+    Instance Attributes:
+    - colour: The target colour for this goal, that is the colour to which
+              this goal applies.
+    """
+    colour: tuple[int, int, int]
+
+    def __init__(self, target_colour: tuple[int, int, int]) -> None:
+        """Initialize this goal to have the given <target_colour>.
+        """
+        self.colour = target_colour
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given <board>.
+
+        The score is always greater than or equal to 0.
+        """
+        raise NotImplementedError
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+        raise NotImplementedError
+
+
+class PerimeterGoal(Goal):
+    """A goal to maximize the presence of this goal's target colour
+    on the board's perimeter.
+    """
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given board.
+
+        The score is always greater than or equal to 0.
+
+        The score for a PerimeterGoal is defined to be the number of unit cells
+        on the perimeter whose colour is this goal's target colour. Corner cells
+        count twice toward the score.
+        """
+        grid = flatten(board)
+        count = 0
+        for i in range(len(grid)):
+            if grid[0][i] == self.colour:
+                count += 1
+            if grid[i][0] == self.colour:
+                count += 1
+            if grid[i][len(grid) - 1] == self.colour:
+                count += 1
+            if grid[len(grid) - 1][i] == self.colour:
+                count += 1
+
+        return count
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+
+        return ('Maximize the presence of ' + colour_name(
+            self.colour) + " on the board's perimeter")
+
+
+class BlobGoal(Goal):
+    """A goal to create the largest connected blob of this goal's target
+    colour, anywhere within the Block.
+    """
+
+    def score(self, board: Block) -> int:
+        """Return the current score for this goal on the given board.
+
+        The score is always greater than or equal to 0.
+
+        The score for a BlobGoal is defined to be the total number of
+        unit cells in the largest connected blob within this Block.
+        """
+        grid = flatten(board)
+        final = []
+        visited = []
+        for _ in range(len(grid)):
+            visited.append([-1] * len(grid))
+
+        for i in range(len(grid)):
+            for j in range(len(grid)):
+                final.append(
+                    self._undiscovered_blob_size((i, j), grid, visited))
+
+        return max(final)
+
+    def _undiscovered_blob_size(self, pos: tuple[int, int],
+                                board: list[list[tuple[int, int, int]]],
+                                visited: list[list[int]]) -> int:
+        """Return the size of the largest connected blob in <board> that (a) is
+        of this Goal's target <colour>, (b) includes the cell at <pos>, and (c)
+        involves only cells that are not in <visited>.
+
+        <board> is the flattened board on which to search for the blob.
+        <visited> is a parallel structure (to <board>) that, in each cell,
+        contains:
+            -1 if this cell has never been visited
+            0  if this cell has been visited and discovered
+               not to be of the target colour
+            1  if this cell has been visited and discovered
+               to be of the target colour
+
+        Update <visited> so that all cells that are visited are marked with
+        either 0 or 1.
+
+        If <pos> is out of bounds for <board>, return 0.
+        """
+        if \
+                pos[0] < 0 or pos[0] >= len(board) or pos[1] < 0 or pos[
+                    1] >= len(board):
+            return 0
+
+        colour = board[pos[0]][pos[1]]
+        count = 0
+        if visited[pos[0]][pos[1]] != -1:
+            return 0
+        if colour != self.colour:
+            visited[pos[0]][pos[1]] = 0
+            return 0
+        else:
+            visited[pos[0]][pos[1]] = 1
+            count += self._undiscovered_blob_size((pos[0] + 1, pos[1]),
+                                                  board, visited)
+            count += self._undiscovered_blob_size((pos[0] - 1, pos[1]),
+                                                  board, visited)
+            count += self._undiscovered_blob_size((pos[0], pos[1] + 1),
+                                                  board, visited)
+            count += self._undiscovered_blob_size((pos[0], pos[1] - 1),
+                                                  board, visited)
+        return count + 1
+
+    def description(self) -> str:
+        """Return a description of this goal.
+        """
+
+        return ('Create the largest connected blob of ' + colour_name(
+            self.colour) + " anywhere within the Block")
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'allowed-import-modules': [
+            'doctest', 'python_ta', 'random', 'typing', 'block', 'settings',
+            'math', '__future__'
+        ],
+        'max-attributes': 15
+    })
---
+++ blocks/a2/player.py
@@ -0,0 +1,349 @@
+"""Player class"""
+
+from __future__ import annotations
+import random
+import pygame
+
+from block import Block
+from goal import Goal, generate_goals
+
+from actions import Action, KEY_ACTION, ROTATE_CLOCKWISE, \
+    ROTATE_COUNTER_CLOCKWISE, \
+    SWAP_HORIZONTAL, SWAP_VERTICAL, SMASH, PASS, PAINT, COMBINE
+
+
+def create_players(num_human: int, num_random: int, smart_players: list[int]) \
+        -> list[Player]:
+    """Return a new list of Player objects.
+
+    <num_human> is the number of human player, <num_random> is the number of
+    random players, and <smart_players> is a list of difficulty levels for each
+    SmartPlayer that is to be created.
+
+    The list should contain <num_human> HumanPlayer objects first, then
+    <num_random> RandomPlayer objects, then the same number of SmartPlayer
+    objects as the length of <smart_players>. The difficulty levels in
+    <smart_players> should be applied to each SmartPlayer object, in order.
+
+    Player ids are given in the order that the players are created, starting
+    at id 0.
+
+    Each player is assigned a random goal.
+    """
+    final = []
+    pid = 0
+    goals = generate_goals(num_human + num_random + len(smart_players))
+    for _ in range(num_human):
+        final.append(HumanPlayer(pid, goals.pop()))
+        pid += 1
+    for _ in range(num_random):
+        final.append(RandomPlayer(pid, goals.pop()))
+        pid += 1
+    for difficulty in smart_players:
+        final.append(SmartPlayer(pid, goals.pop(), difficulty))
+        pid += 1
+
+    return final
+
+
+def _get_block(block: Block, location: tuple[int, int], level: int) -> \
+        Block | None:
+    """Return the Block within <block> that is at <level> and includes
+    <location>. <location> is a coordinate-pair (x, y).
+
+    A block includes all locations that are strictly inside it, as well as
+    locations on the top and left edges. A block does not include locations that
+    are on the bottom or right edge.
+
+    If a Block includes <location>, then so do its ancestors. <level> specifies
+    which of these blocks to return. If <level> is greater than the level of
+    the deepest block that includes <location>, then return that deepest block.
+
+    If no Block can be found at <location>, return None.
+
+    Preconditions:
+        - block.level <= level <= block.max_depth
+    """
+    bx = block.position[0]
+    by = block.position[1]
+    lx = location[0]
+    ly = location[1]
+    if not (bx <= lx < (bx + block.size) and by <= ly < (by + block.size)):
+        return None
+    elif level == block.level:
+        return block
+    elif block.colour is not None:
+        return block
+    else:
+        for child in block.children:
+            if _get_block(child, location, level) is not None:
+                return _get_block(child, location, level)
+    return None
+
+
+class Player:
+    """A player in the Blocky game.
+
+    This is an abstract class. Only child classes should be instantiated.
+
+    Instance Attributes:
+    - id: This player's number.
+    - goal: This player's assigned goal for the game.
+    - penalty: The penalty accumulated by this player through their actions.
+    """
+    id: int
+    goal: Goal
+    penalty: int
+
+    def __init__(self, player_id: int, goal: Goal) -> None:
+        """Initialize this Player.
+        """
+        self.goal = goal
+        self.id = player_id
+        self.penalty = 0
+
+    def get_selected_block(self, board: Block) -> Block | None:
+        """Return the block that is currently selected by the player.
+
+        If no block is selected by the player, return None.
+        """
+        raise NotImplementedError
+
+    def process_event(self, event: pygame.event.Event) -> None:
+        """Update this player based on the pygame event.
+        """
+        raise NotImplementedError
+
+    def generate_move(self, board: Block) -> \
+            tuple[Action, Block] | None:
+        """Return a potential move to make on the <board>.
+
+        The move is a tuple consisting of an action and
+        the block the action will be applied to.
+
+        Return None if no move can be made, yet.
+        """
+        raise NotImplementedError
+
+
+class HumanPlayer(Player):
+    """A human player.
+
+    Instance Attributes:
+    - _level: The level of the Block that the user selected most recently.
+    - _desired_action: The most recent action that the user is attempting to do.
+
+    Representation Invariants:
+    - self._level >= 0
+    """
+    _level: int
+    _desired_action: Action | None
+
+    def __init__(self, player_id: int, goal: Goal) -> None:
+        """Initialize this HumanPlayer with the given <renderer>, <player_id>
+        and <goal>.
+        """
+        Player.__init__(self, player_id, goal)
+
+        # This HumanPlayer has not yet selected a block, so set _level to 0
+        # and _selected_block to None.
+        self._level = 0
+        self._desired_action = None
+
+    def get_selected_block(self, board: Block) -> Block | None:
+        """Return the block that is currently selected by the player based on
+        the position of the mouse on the screen and the player's desired level.
+
+        If no block is selected by the player, return None.
+        """
+        mouse_pos = pygame.mouse.get_pos()
+        block = _get_block(board, mouse_pos, self._level)
+
+        return block
+
+    def process_event(self, event: pygame.event.Event) -> None:
+        """Respond to the relevant keyboard events made by the player based on
+        the mapping in KEY_ACTION, as well as the W and S keys for changing
+        the level.
+        """
+        if event.type == pygame.KEYUP:
+            if event.key in KEY_ACTION:
+                self._desired_action = KEY_ACTION[event.key]
+            elif event.key == pygame.K_w:
+                self._level -= 1
+                self._desired_action = None
+            elif event.key == pygame.K_s:
+                self._level += 1
+                self._desired_action = None
+
+    def generate_move(self, board: Block) -> \
+            tuple[Action, Block] | None:
+        """Return the move that the player would like to perform. The move may
+        not be valid.
+
+        Return None if the player is not currently selecting a block.
+
+        This player's desired action gets reset after this method is called.
+        """
+        block = self.get_selected_block(board)
+
+        if block is None or self._desired_action is None:
+            self._correct_level(board)
+            self._desired_action = None
+            return None
+        else:
+            move = self._desired_action, block
+
+            self._desired_action = None
+            return move
+
+    def _correct_level(self, board: Block) -> None:
+        """Correct the level of the block that the player is currently
+        selecting, if necessary.
+        """
+        self._level = max(0, min(self._level, board.max_depth))
+
+
+class ComputerPlayer(Player):
+    """A computer player. This class is still abstract,
+    as how it generates moves is still to be defined
+    in a subclass.
+
+    Instance Attributes:
+    - _proceed: True when the player should make a move, False when the
+                player should wait.
+    """
+    _proceed: bool
+
+    def __init__(self, player_id: int, goal: Goal) -> None:
+        Player.__init__(self, player_id, goal)
+
+        self._proceed = False
+
+    def get_selected_block(self, board: Block) -> Block | None:
+        return None
+
+    def process_event(self, event: pygame.event.Event) -> None:
+        if (event.type == pygame.MOUSEBUTTONDOWN
+                and event.button == pygame.BUTTON_LEFT):
+            self._proceed = True
+
+    # Note: this is included just to make pyTA happy; as it thinks
+    #       we forgot to implement this abstract method otherwise :)
+    def generate_move(self, board: Block) -> \
+            tuple[Action, Block] | None:
+        raise NotImplementedError
+
+
+class RandomPlayer(ComputerPlayer):
+    """A computer player who chooses completely random moves."""
+
+    def generate_move(self, board: Block) -> \
+            tuple[Action, Block] | None:
+        """Return a valid, randomly generated move only during the player's
+        turn.  Return None if the player should not make a move yet.
+
+        A valid move is a move other than PASS that can be successfully
+        performed on the <board>.
+
+        This function does not mutate <board>.
+        """
+        if self._proceed is True:
+            while self._proceed:
+                new_board = board.create_copy()
+                moves = [ROTATE_CLOCKWISE, ROTATE_COUNTER_CLOCKWISE,
+                         SWAP_HORIZONTAL, SWAP_VERTICAL, SMASH,
+                         PAINT, COMBINE]
+                actions = random.choice(moves)
+                x = random.randint(new_board.position[0], board.size - 1)
+                y = random.randint(new_board.position[1], board.size - 1)
+                level = random.randint(new_board.level, new_board.max_depth)
+                actual_move = _get_block(board, (x, y), level)
+                copy_move = _get_block(new_board, (x, y), level)
+                if actions.apply(copy_move, {'colour': self.goal.colour}):
+                    self._proceed = False
+                    return actions, actual_move
+        return None
+
+
+class SmartPlayer(ComputerPlayer):
+    """A computer player who chooses moves by assessing a series of random
+    moves and choosing the one that yields the best score.
+
+    Private Instance Attributes:
+    - _num_test: The number of moves this SmartPlayer will test out before
+                 choosing a move.
+    """
+    _num_test: int
+
+    def __init__(self, player_id: int, goal: Goal, difficulty: int) -> None:
+        """Initialize this SmartPlayer with a <player_id> and <goal>.
+
+        Use <difficulty> to determine and record how many moves this SmartPlayer
+        will assess before choosing a move. The higher the value for
+        <difficulty>, the more moves this SmartPlayer will assess, and hence the
+        more difficult an opponent this SmartPlayer will be.
+
+        Preconditions:
+        - difficulty >= 0
+        """
+        ComputerPlayer.__init__(self, player_id, goal)
+        self._num_test = difficulty
+
+    def generate_move(self, board: Block) -> \
+            tuple[Action, Block] | None:
+        """Return a valid move only during the player's turn by assessing
+        multiple valid moves and choosing the move that results in the highest
+        score for this player's goal.  This score should also account for the
+        penalty of the move.  Return None if the player should not make a move.
+
+        A valid move is a move other than PASS that can be successfully
+        performed on the <board>. If no move can be found that is better than
+        the current score, this player will pass.
+
+        This method does not mutate <board>.
+        """
+        total = []
+        if self._proceed is True:
+            for _ in range(self._num_test):
+                new_board = board.create_copy()
+                moves = [ROTATE_CLOCKWISE, ROTATE_COUNTER_CLOCKWISE,
+                         SWAP_HORIZONTAL, SWAP_VERTICAL, SMASH,
+                         PAINT, COMBINE]
+                actions = random.choice(moves)
+                x = random.randint(new_board.position[0], board.size - 1)
+                y = random.randint(new_board.position[1], board.size - 1)
+                level = random.randint(new_board.level, new_board.max_depth)
+                actual_move = _get_block(board, (x, y), level)
+                copy_move = _get_block(new_board, (x, y), level)
+                if actions.apply(copy_move, {'colour': self.goal.colour}):
+                    total.append((actions, actual_move,
+                                  self.goal.score(new_board)))
+            i = 0
+            best_score = self.goal.score(board)
+            best_move = None
+            while i < len(total):
+                if (total[i][2] - total[i][0].penalty) > best_score:
+                    best_score = total[i][2] - total[i][0].penalty
+                    best_move = total[i][0], total[i][1]
+                i += 1
+            self._proceed = False
+            if best_score <= self.goal.score(board):
+                return PASS, board
+            else:
+                return best_move
+        return None
+
+
+if __name__ == '__main__':
+    import python_ta
+
+    python_ta.check_all(config={
+        'allowed-io': ['process_event'],
+        'allowed-import-modules': [
+            'doctest', 'python_ta', 'random', 'typing', 'actions', 'block',
+            'goal', 'pygame', '__future__'
+        ],
+        'max-attributes': 10,
+        'generated-members': 'pygame.*'
+    })
